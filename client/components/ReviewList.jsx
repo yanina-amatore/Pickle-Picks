09c64499cc0355fe617deb4aae8315ec9b7572b0
Yanina <99714877+yanina-amatore@users.noreply.github.com>
2022-09-15 14:25:26 +1200

    showing reviewlist & home for dev branch merge

--- client/components/ReviewList.jsx
+++ client/components/ReviewList.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { useStore } from 'react-redux'
 
 import { fetchReview } from '../actions/review'
 
 // Data needs to come from Reviewlist prop
-function Reviewlist(data) {
+function ReviewList(data) {
   // const dispatch = useDispatch()
 
   const reviews = useSelector((state) => state.reviews)
@@ -13,13 +12,7 @@ function Reviewlist(data) {
   return (
 <>
     <div>
-      {reviews.map((review, idx) => {
-        return (
-          // review.location, review.
-        )
-      })}
-
-
+     
 
       <h2>Name</h2>
       <p> Rating </p>
