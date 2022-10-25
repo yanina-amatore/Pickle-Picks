23004ee90dc5421d9da55d8596569a087157eeb3
Yanina Amatore <yaninaamatore@gmail.com>
2022-10-25 13:40:54 +1300

    Login component style fixed

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -33,44 +33,57 @@ function Login() {
   }
 
   return (
-    <form className="form box" onSubmit={handleSubmit}>
-      <h1 className="title is-2">Login</h1>
-      <hr />
-      {auth.errorMessage && (
-        <span className="has-text-danger is-large">{auth.errorMessage}</span>
-      )}
-      <label className="label is-large has-text-centered">
-        Username
-        <input
-          required
-          className="input has-text-centered is-large is-fullwidth"
-          placeholder="User Name"
-          type="text"
-          name="username"
-          autoComplete="username"
-          value={formData.username}
-          onChange={handleChange}
-        />
-      </label>
-      <label className="label is-large has-text-centered">
-        Password
-        <input
-          required
-          className="input has-text-centered is-large is-fullwidth"
-          placeholder="Password"
-          type="password"
-          name="password"
-          autoComplete="current-password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-      </label>
-      <input
-        className="button is-large is-fullwidth is-success"
-        value="Login"
-        type="submit"
-      />
-    </form>
+    <div className='section'>
+      <form className="form box  py-6" onSubmit={handleSubmit}>
+        <h1 className="title is-2 mx-6 p-2">Login</h1>
+        <hr className='mx-6' />
+        {auth.errorMessage && (
+          <span className="has-text-danger is-large">{auth.errorMessage}</span>
+        )}
+        <div className="container  mx-6 p-2" >
+          <div className='field control has-icons-left has-icons-right'>
+            <input
+              required
+              className="input has-text-centered is-large is-fullwidth"
+              placeholder="User Name"
+              type="text"
+              name="username"
+              autoComplete="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+            <span className="icon is-small is-left">
+              <i className="fas fa-user"></i>
+            </span>
+          </div>
+          <div className='field control has-icons-left has-icons-right'>
+            <input
+              required
+              className="input has-text-centered is-large is-fullwidth"
+              placeholder="Password"
+              type="password"
+              name="password"
+              autoComplete="current-password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+            <span className="icon is-small is-left">
+              <i className="fas fa-lock"></i>
+            </span>
+          </div>
+          <div className='field'>
+            <input
+              className="button is-medium  is-success"
+              value="Login"
+              type="submit"
+            />
+
+          </div>
+        </div>
+      </form>
+
+    </div>
+
   )
 }
 
